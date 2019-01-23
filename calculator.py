36039f059bd68f0dc4e1bebc88337cb17d8395f5
arturo jimenez <arturo.jimenezucv@gmail.com>
2019-01-22 21:07:07 -0500

    Making add() a function that receives multiples parameters

--- calculator.py
+++ calculator.py
@@ -1,5 +1,8 @@
-def add(a = 0 , b = 0):
-    return a + b
+def add(*args):
+    answer = 0
+    for item in args:
+        answer += item
+    return answer
 
 def subs(a, b):
     return a - b
--- tests/test_add.py
+++ tests/test_add.py
@@ -9,5 +9,8 @@ def test_add():
 def test_no_parameters():
     assert add() == 0
 
+def test_mult_parameters():
+    assert add(4,5,6,10) == 25
+
 def test_subs():
     assert subs(6,2) == 4
