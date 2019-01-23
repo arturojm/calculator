ec6dba6a141a1f986286769afa724b333c5fdb7d
arturo jimenez <arturo.jimenezucv@gmail.com>
2019-01-22 21:25:05 -0500

    Adding decimal testing

--- tests/test_add.py
+++ tests/test_add.py
@@ -1,6 +1,7 @@
 """
 Test the add() function of the calculator
 """
+import pytest
 from calculator import *
 
 def test_add():
@@ -11,6 +12,9 @@ def test_no_parameters():
 
 def test_mult_parameters():
     assert add(4,5,6,10) == 25
+
+def test_decimal():
+    assert add(0.1, 0.1, 0.1) == pytest.approx(0.3)
 
 def test_subs():
     assert subs(6,2) == 4
