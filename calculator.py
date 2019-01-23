d91a8d20e8ac57391ad3f28c6db6c2604176fe56
arturo jimenez <arturo.jimenezucv@gmail.com>
2019-01-22 20:58:31 -0500

    Making add() function to optional parameters

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
-def add(a , b):
+def add(a = 0 , b = 0):
     return a + b
 
 def subs(a, b):
-    return a - b 
+    return a - b
--- tests/test_add.py
+++ tests/test_add.py
@@ -6,5 +6,8 @@ from calculator import *
 def test_add():
     assert add(5,3) == 8
 
+def test_no_parameters():
+    assert add() == 0
+
 def test_subs():
     assert subs(6,2) == 4
